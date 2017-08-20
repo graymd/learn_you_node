115b014ccb12219f58c4fec21f1a567603c410ec
Malachi Gray <graymd.cfi@gmail.com>
2017-08-20 13:43:53 -0400

    l13 complete

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -3,6 +3,7 @@ const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
   console.log(req.name);
+  req.flash('error', 'Something Happened');
   res.render('index');
 };
 
@@ -11,8 +12,16 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();  //don't move on until save has happened
-  res.redirect('/');
+  console.log(req.body);
+  const store = await (new Store(req.body)).save();
+  //await don't move on until save has happened
+  req.flash('success', `Successfully Created ${store.name}.  Care leave a review?`);
+  res.redirect(`/${store.slug}`);
 
 };
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', { title: 'Stores', stores })
+}
