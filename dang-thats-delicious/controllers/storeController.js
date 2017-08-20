70aa6740a30b1d159734396940df9810c4a7f93b
Malachi Gray <graymd.cfi@gmail.com>
2017-08-20 13:01:47 -0400

    working thru lesson 12

---
+++ dang-thats-delicious/controllers/storeController.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => {
+  console.log(req.name);
+  res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();  //don't move on until save has happened
+  res.redirect('/');
+
+};
