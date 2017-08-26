8ad0b0b95f917bd93dfa4cc610f66109a01b6f74
Malachi Gray <graymd.cfi@gmail.com>
2017-08-25 21:40:04 -0400

    l21 complete

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -51,5 +51,12 @@ if(!this.isModified('name')) {
   next();
 });
 
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: `$tags`, count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
+};
 
 module.exports = mongoose.model('Store', storeSchema);
