894c34635e3dcf6755ba2ab3c9142b7d77cfaa0a
Malachi Gray <graymd.cfi@gmail.com>
2017-08-26 14:33:08 -0400

    l24 complete

--- dang-thats-delicious/models/User.js
+++ dang-thats-delicious/models/User.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 const md5 = require('md5');
 const validator = require('validator');
 const mongodbErrorHandler = require('mongoose-mongodb-errors');
-const passportLocalMongoose = require('password-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
   email: {
@@ -14,7 +14,7 @@ const userSchema = new Schema({
     trim: true,
     validate: [validator.isEmail, 'Invalid Email Address'],
     required: 'Please supply an email address'
-  }
+  },
   name: {
     type: String,
     required: 'Please supply a name',
